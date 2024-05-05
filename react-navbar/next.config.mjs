8a50221f316df333b6319eb00a76784f8cd0a495
xwshiba <xw2357@columbia.edu>
2024-05-04 23:16:59 -0700

    fix path

--- react-navbar/next.config.mjs
+++ react-navbar/next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     basePath: '', // Set the basePath to your desired value
     output: "export",  // <=== enables static exports
+    assetPrefix: isProd ? '/react-navbar' : undefined,
 };
 
 export default nextConfig;
